61d633a0a0ae626294698e56b6a8a4630215c34a
TheSpaceSheep <noe.malais@gmail.com>
2020-06-26 15:48:24 +0200

    modify test file

--- test_ppo_mg.py
+++ test_ppo_mg.py
@@ -8,15 +8,15 @@ import microgridRLsimulator
 
 env = gym.make("microgridRLsimulator-v0",
                start_date="2016-01-01",
-               end_date="2016-01-31",
+               end_date="2017-07-31",
                case="elespino")
 
 model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=50000)
 
 obs = env.reset()
 
-for i in range(100):
+for i in range(10000):
     action, _states = model.predict(obs)
     obs, reward, dones, info = env.step(action)
     time.sleep(0.1)
