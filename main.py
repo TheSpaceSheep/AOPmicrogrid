c8c1a1e438624d20e47428915c7b8bb71599b6b7
TheSpaceSheep <noe.malais@gmail.com>
2020-06-30 11:03:16 +0200

    write main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#!usr/bin/python
+import argparse
+import copy
+import microgridRLsimulator
+
+import params.env_params as env_params
+from agents.random_agent import RandomAgent
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', '-e', type=str, default='microgrid',
+                    choices=['microgrid', 'maze-dense', 'maze-sparse'])
+
+args = parser.parse_args()
+
+params = copy.deepcopy(env_params.env_params)
+params['env']['env'] = args.env
+
+
+agent = RandomAgent(params)
+env = agent.env
+env.reset()
+
+for i in range(100):
+    action = agent.get_action()
+    env.step(action)
+    print("Taking action", action)
